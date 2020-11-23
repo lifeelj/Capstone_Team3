c25589c8a20b494202706b1c83a16605f7f0cc8a
haan823 <57613635+haan823@users.noreply.github.com>
2020-11-23 19:14:09 +0900

    배포 전 최종 버전

--- fitts/urls.py
+++ fitts/urls.py
@@ -5,5 +5,11 @@ app_name = 'fitts'
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('fitts_task', views.fitts, name='fitts'),
+    path('fitts_task/1', views.fitts1, name='fitts1'),
+    path('fitts_task/2', views.fitts2, name='fitts2'),
+    path('fitts_task/3', views.fitts3, name='fitts3'),
+    path('fitts_task/4', views.fitts4, name='fitts4'),
+    path('fitts_task/5', views.fitts5, name='fitts5'),
+    path('fitts_task/6', views.fitts6, name='fitts6'),
+    path('fitts_task/end', views.end, name='end')
 ]
--- fitts/views.py
+++ fitts/views.py
@@ -10,11 +10,30 @@ def login(request):
         age = request.POST.get('age')
         request.session['email']=email
         request.session['age']=age
-        return redirect('fitts:fitts')
+        return redirect('fitts:fitts1')
     elif request.method == "GET":
         return render(request, 'fitts/login.html')
 
-def fitts(request):
+def fitts1(request):
+    if request.method == "POST":
+        for i in range(1, 9):
+            age = request.session.get('age', None)
+            email = request.session.get('email', None)
+            transform = request.POST.get('transform', None)
+            distance = request.POST.get('distance' + str(i), None)
+            time = request.POST.get('time' + str(i), None)
+            Tasks.objects.create(
+                age = age,
+                email = email,
+                transform = transform,
+                distance = distance,
+                time = time
+            )
+        return redirect('fitts:fitts2')
+    elif request.method == "GET":
+        return render(request, 'fitts/fittstask1.html',{})
+
+def fitts2(request):
     if request.method == "POST":
         for i in range(1, 9):
             age = request.session.get('age')
@@ -29,6 +48,85 @@ def fitts(request):
                 distance = distance,
                 time = time
             )
-        return redirect('fitts:fitts')
+        return redirect('fitts:fitts3')
     elif request.method == "GET":
-        return render(request, 'fitts/fittstask.html',{})
+        return render(request, 'fitts/fittstask2.html',{})
+
+def fitts3(request):
+    if request.method == "POST":
+        for i in range(1, 9):
+            age = request.session.get('age')
+            email = request.session.get('email')
+            transform = request.POST.get('transform', None)
+            distance = request.POST.get('distance' + str(i), None)
+            time = request.POST.get('time' + str(i), None)
+            Tasks.objects.create(
+                age = age,
+                email = email,
+                transform = transform,
+                distance = distance,
+                time = time
+            )
+        return redirect('fitts:fitts4')
+    elif request.method == "GET":
+        return render(request, 'fitts/fittstask3.html',{})
+
+def fitts4(request):
+    if request.method == "POST":
+        for i in range(1, 9):
+            age = request.session.get('age')
+            email = request.session.get('email')
+            transform = request.POST.get('transform', None)
+            distance = request.POST.get('distance' + str(i), None)
+            time = request.POST.get('time' + str(i), None)
+            Tasks.objects.create(
+                age = age,
+                email = email,
+                transform = transform,
+                distance = distance,
+                time = time
+            )
+        return redirect('fitts:fitts5')
+    elif request.method == "GET":
+        return render(request, 'fitts/fittstask4.html',{})
+
+def fitts5(request):
+    if request.method == "POST":
+        for i in range(1, 9):
+            age = request.session.get('age')
+            email = request.session.get('email')
+            transform = request.POST.get('transform', None)
+            distance = request.POST.get('distance' + str(i), None)
+            time = request.POST.get('time' + str(i), None)
+            Tasks.objects.create(
+                age = age,
+                email = email,
+                transform = transform,
+                distance = distance,
+                time = time
+            )
+        return redirect('fitts:fitts6')
+    elif request.method == "GET":
+        return render(request, 'fitts/fittstask5.html',{})
+
+def fitts6(request):
+    if request.method == "POST":
+        for i in range(1, 9):
+            age = request.session.get('age')
+            email = request.session.get('email')
+            transform = request.POST.get('transform', None)
+            distance = request.POST.get('distance' + str(i), None)
+            time = request.POST.get('time' + str(i), None)
+            Tasks.objects.create(
+                age = age,
+                email = email,
+                transform = transform,
+                distance = distance,
+                time = time
+            )
+        return redirect('fitts:end')
+    elif request.method == "GET":
+        return render(request, 'fitts/fittstask6.html',{})
+
+def end(request):
+    return render(request, 'fitts/end.html')
